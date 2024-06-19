56ab5fee3c6fbb52f3be0802558a944fc867d97a
M.bek <joraboyevmustafo3.gmail.com>
2024-06-19 23:28:25 +0500

    testiomonials

--- src/Component/Testimonials/Testimonials.jsx
+++ src/Component/Testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Testimonials.css'
 import next_icon from '../../assets/next-icon.png'
 import back_icon from '../../assets/back-icon.png'
@@ -8,10 +8,28 @@ import user_3 from '../../assets/user-3.png'
 import user_4 from '../../assets/user-4.png'
 
 const Testimonials = () => {
+
+        const slider = useRef();
+        let tx = 0;
+
+    const slideForward = () => {
+        if(tx > -50){
+            tx -= 25;   
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    } 
+
+    const slideBackward = () => {
+        if(tx > 0){
+            tx += 25;   
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    } 
+
   return (
     <div className='testimonials'>
-        <img className='next-btn' src={next_icon} alt="" />
-        <img className='back-btn' src={back_icon} alt="" />
+        <img className='next-btn' src={next_icon} alt="" onClick={slideForward} />
+        <img className='back-btn' src={back_icon} alt="" onClick={slideBackward} />
         <div className="slider">
             <ul>
                 <li>
@@ -36,7 +54,7 @@ const Testimonials = () => {
                         <div className="user-info">
                             <img src={user_2} alt="" />
                             <div>
-                                <h3>William Jackson</h3>
+                                <h3>William Jackson 1</h3>
                                 <span>Edusity, USA</span>
                             </div>
                         </div>
@@ -53,7 +71,7 @@ const Testimonials = () => {
                         <div className="user-info">
                             <img src={user_3} alt="" />
                             <div>
-                                <h3>William Jackson</h3>
+                                <h3>William Jackson 2</h3>
                                 <span>Edusity, USA</span>
                             </div>
                         </div>
@@ -70,7 +88,7 @@ const Testimonials = () => {
                         <div className="user-info">
                             <img src={user_4} alt="" />
                             <div>
-                                <h3>William Jackson</h3>
+                                <h3>William Jackson 3</h3>
                                 <span>Edusity, USA</span>
                             </div>
                         </div>
