6ab726f775b56540b116c401749f0bc90cee8249
M.bek <joraboyevmustafo3.gmail.com>
2024-06-26 14:01:13 +0500

    slide fixed

--- src/Component/Testimonials/Testimonials.jsx
+++ src/Component/Testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import './Testimonials.css'
 import next_icon from '../../assets/next-icon.png'
 import back_icon from '../../assets/back-icon.png'
@@ -8,102 +8,114 @@ import user_3 from '../../assets/user-3.png'
 import user_4 from '../../assets/user-4.png'
 
 const Testimonials = () => {
-
-        const slider = useRef();
-        let tx = 0;
+    const slider = useRef();
+    const tx = useRef(0);
+    const slideCount = 4; // Slaydlar soni
+    const slideWidth = 100 / slideCount; // Har bir slaydning kengligi
 
     const slideForward = () => {
-        if(tx > -50){
-            tx -= 25;   
+        if (tx.current > -slideWidth * (slideCount - 1)) {
+            tx.current -= slideWidth;   
+        } else {
+            tx.current = 0; // Qayta boshlash
         }
-        slider.current.style.transform = `translateX(${tx}%)`;
-    } 
+        slider.current.style.transform = `translateX(${tx.current}%)`;
+    }
 
     const slideBackward = () => {
-        if(tx > 0){
-            tx += 25;   
+        if (tx.current < 0) {
+            tx.current += slideWidth;   
+        } else {
+            tx.current = -slideWidth * (slideCount - 1); // Oxirgi slaydga o'tish
         }
-        slider.current.style.transform = `translateX(${tx}%)`;
-    } 
+        slider.current.style.transform = `translateX(${tx.current}%)`;
+    }
 
-  return (
-    <div className='testimonials'>
-        <img className='next-btn' src={next_icon} alt="" onClick={slideForward} />
-        <img className='back-btn' src={back_icon} alt="" onClick={slideBackward} />
-        <div className="slider">
-            <ul>
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_1} alt="" />
-                            <div>
-                                <h3>William Jackson</h3>
-                                <span>Edusity, USA</span>
+    useEffect(() => {
+        const interval = setInterval(slideForward, 3000); // Har 3 soniyada slaydni o'tkazish
+        return () => clearInterval(interval); // Tozalash
+    }, []);
+
+    return (
+        <div className='testimonials'>
+            <img className='next-btn' src={next_icon} alt="" onClick={slideForward} />
+            <img className='back-btn' src={back_icon} alt="" onClick={slideBackward} />
+            <div className="slider">
+                <ul ref={slider}>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_1} alt="" />
+                                <div>
+                                    <h3>William Jackson</h3>
+                                    <span>Edusity, USA</span>
+                                </div>
                             </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing 
+                                elit. Reprehenderit, ipsam. 
+                                Magnam nostrum nulla dolores praesentium debitis 
+                                molestiae. Nulla, perferendis a, accusantium 
+                                architecto tempora provident laudantium nisi, 
+                                non vel praesentium deserunt?</p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing 
-                            elit. Reprehenderit, ipsam. 
-                            Magnam nostrum nulla dolores praesentium debitis 
-                            molestiae. Nulla, perferendis a, accusantium 
-                            architecto tempora provident laudantium nisi, 
-                            non vel praesentium deserunt?</p>
-                    </div>
-                </li>
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_2} alt="" />
-                            <div>
-                                <h3>William Jackson 1</h3>
-                                <span>Edusity, USA</span>
+                    </li>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_2} alt="" />
+                                <div>
+                                    <h3>William Jackson 1</h3>
+                                    <span>Edusity, USA</span>
+                                </div>
                             </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing 
+                                elit. Reprehenderit, ipsam. 
+                                Magnam nostrum nulla dolores praesentium debitis 
+                                molestiae. Nulla, perferendis a, accusantium 
+                                architecto tempora provident laudantium nisi, 
+                                non vel praesentium deserunt?</p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing 
-                            elit. Reprehenderit, ipsam. 
-                            Magnam nostrum nulla dolores praesentium debitis 
-                            molestiae. Nulla, perferendis a, accusantium 
-                            architecto tempora provident laudantium nisi, 
-                            non vel praesentium deserunt?</p>
-                    </div>
-                </li>
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_3} alt="" />
-                            <div>
-                                <h3>William Jackson 2</h3>
-                                <span>Edusity, USA</span>
+                    </li>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_3} alt="" />
+                                <div>
+                                    <h3>William Jackson 2</h3>
+                                    <span>Edusity, USA</span>
+                                </div>
                             </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing 
+                                elit. Reprehenderit, ipsam. 
+                                Magnam nostrum nulla dolores praesentium debitis 
+                                molestiae. Nulla, perferendis a, accusantium 
+                                architecto tempora provident laudantium nisi, 
+                                non vel praesentium deserunt?</p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing 
-                            elit. Reprehenderit, ipsam. 
-                            Magnam nostrum nulla dolores praesentium debitis 
-                            molestiae. Nulla, perferendis a, accusantium 
-                            architecto tempora provident laudantium nisi, 
-                            non vel praesentium deserunt?</p>
-                    </div>
-                </li>
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_4} alt="" />
-                            <div>
-                                <h3>William Jackson 3</h3>
-                                <span>Edusity, USA</span>
+                    </li>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_4} alt="" />
+                                <div>
+                                    <h3>William Jackson 3</h3>
+                                    <span>Edusity, USA</span>
+                                </div>
                             </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing 
+                                elit. Reprehenderit, ipsam. 
+                                Magnam nostrum nulla dolores praesentium debitis 
+                                molestiae. Nulla, perferendis a, accusantium 
+                                architecto tempora provident laudantium nisi, 
+                                non vel praesentium deserunt?</p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing 
-                            elit. Reprehenderit, ipsam. 
-                            Magnam nostrum nulla dolores praesentium debitis 
-                            molestiae. Nulla, perferendis a, accusantium 
-                            architecto tempora provident laudantium nisi, 
-                            non vel praesentium deserunt?</p>
-                    </div>
-                </li>
-            </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export default Testimonials
+export default Testimonials;
+
+
