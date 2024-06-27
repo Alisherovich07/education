b0befed5421e308621951d3e87d7fa92b9b46d27
M.bek <joraboyevmustafo3.gmail.com>
2024-06-28 02:03:14 +0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Component/Navbar/Navbar'
 import Hero from './Component/Hero/Hero'
 import Programs from './Component/Programs/Programs'
@@ -6,8 +6,14 @@ import Title from './Component/Title/Title'
 import About from './Component/About/About'
 import Campus from './Component/Campus/Campus'
 import Testimonials from './Component/Testimonials/Testimonials'
+import Contact from './Component/Contact/Contact'
+import Footer from './Component/Footer/Footer'
+import VideoPlayer from './Component/VideoPlayer/VideoPlayer'
 
 const App = () => {
+
+  const [playState, setPlayState] = useState(false)
+
   return (
     <div>
       <Navbar />
@@ -15,13 +21,16 @@ const App = () => {
       <div className="container">
       <Title subTitle='Our PROGRAM' title='What We Offer' />
       <Programs />  
-      <About />
+      <About setPlayState={setPlayState} />
       <Title subTitle='Gallery' title='Campus Photos' />
       <Campus />
       <Title subTitle='TESTIMONIALS' title='What Student Says ' />
       <Testimonials />
+      <Title subTitle='Contact Us' title='Get in Touch' />
+      <Contact />
+      <Footer />
       </div>
-      
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
   )
 }
